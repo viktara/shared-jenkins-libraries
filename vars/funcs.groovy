fe6e6c1e32a8e1b5cf1fb5bf73cd6b7279c78e4f
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2018-08-10 21:54:04 +0000

    Durability.

--- vars/funcs.groovy
+++ vars/funcs.groovy
@@ -19,7 +19,7 @@ def combo(task, axes) {
 }
 
 def durable() {
-    System.setProperty("org.jenkinsci.plugins.durabletask.BourneShellScript.HEARTBEAT_CHECK_INTERVAL", 3600)
+    System.setProperty("org.jenkinsci.plugins.durabletask.BourneShellScript.HEARTBEAT_CHECK_INTERVAL", "3600")
 }
 
 def dnfInstall(deps) {
