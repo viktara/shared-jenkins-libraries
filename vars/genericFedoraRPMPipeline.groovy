7e0e54ef92fa26efddcaa51d9d5611c3ce26c927
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2019-03-13 20:53:21 +0000

    Use build slave now.

--- vars/genericFedoraRPMPipeline.groovy
+++ vars/genericFedoraRPMPipeline.groovy
@@ -145,125 +145,146 @@ def call(checkout_step = null, srpm_step = null) {
 					}
 				}
 			}
-			stage('Checkout') {
-				steps {
-					sh 'rm -f xunit.xml'
-					dir('src') {
-						checkout scm
-						sh 'git clean -fxd'
-					}
-					script {
-						if (checkout_step != null) {
-							checkout_step()
-						}
-					}
-				}
-			}
-			stage('Test') {
-				steps {
-					dir('src') {
-						sh '''
-							set -e
-							if test -f setup.py ; then
-								if [ $(find . -name '*_test.py' -o -name 'test_*.py' | wc -l) != 0 ] ; then
-									nosetests -v --with-xunit --xunit-file=../xunit.xml
-								fi
-							fi
-						'''
-					}
-				}
-			}
-			stage('SRPM') {
-				steps {
-					script {
-						if (srpm_step != null) {
-							srpm_step()
-						} else {
+			stage('Dispatch') {
+				agent { label 'mock' }
+				stages {
+					stage('Checkout') {
+						steps {
+							sh 'rm -f xunit.xml'
 							dir('src') {
-								if (fileExists('setup.py')) {
-									sh '''
-										set -e
-										rm -rf build dist
-										if head -1 setup.py | grep -q python3 ; then
-											python=python3
-										else
-											python=python2
-										fi
-										$python setup.py sdist
-										$python setup.py bdist_rpm --spec-only
-										rpmbuild --define "_srcrpmdir ./" --define "_sourcedir dist/" -bs dist/*.spec
-										rm -rf build dist
-									'''
-								} else if (fileExists('pypipackage-to-srpm.yaml')) {
-									sh '''
-										y=pypipackage-to-srpm.yaml
-										url=$(shyaml get-value url < $y)
-										fn=$(basename "$url")
-										sha256sum=$(shyaml get-value sha256sum < $y)
-										mangle_name=
-										if [ "$(shyaml get-value mangle_name True < $y)" == "False" ] ; then
-											mangle_name=--no-mangle-name
-										fi
-										epoch=$(shyaml get-value epoch < $y || true)
-										if [ "$epoch" != "" ] ; then
-											epoch="--epoch=$epoch"
-										fi
-										python_versions=$(shyaml get-values python_versions < $y || true)
-										if [ "$python_versions" == "" ] ; then
-											python_versions="2 3"
-										fi
-										if [ "$python_versions" == "2 3" -o "$python_versions" == "3 2" ] ; then
-											if [ "$mangle_name" == "--no-mangle-name" ] ; then
-												>&2 echo error: cannot build for two Python versions without mangling the name of the package
-												exit 36
-											fi
-										fi
-										diffs=1
-										for f in *.diff ; do
-											test -f "$f" || diffs=0
-										done
-										wget --progress=dot:giga --timeout=15 -O "$fn" "$url"
-										actualsum=$(sha256sum "$fn" | cut -d ' ' -f 1)
-										if [ "$actualsum" != "$sha256sum" ] ; then
-											>&2 echo error: SHA256 sum "$actualsum" of file "$fn" does not match expected sum "$sha256sum"
-											exit 32
-										fi
-										for v in $python_versions ; do
-											if [ "$diffs" == "1" ] ; then
-												python"$v" `which pypipackage-to-srpm` --no-binary-rpms $epoch $mangle_name "$fn" *.diff
-											else
-												python"$v" `which pypipackage-to-srpm` --no-binary-rpms $epoch $mangle_name "$fn"
-											fi
-										done
-									'''
-								} else {
-									sh 'make srpm'
+								checkout scm
+								sh 'git clean -fxd'
+							}
+							script {
+								if (checkout_step != null) {
+									checkout_step()
 								}
 							}
 						}
 					}
+					stage('Test') {
+						steps {
+							dir('src') {
+								sh '''
+									set -e
+									if test -f setup.py ; then
+										if [ $(find . -name '*_test.py' -o -name 'test_*.py' | wc -l) != 0 ] ; then
+											nosetests -v --with-xunit --xunit-file=../xunit.xml
+										fi
+									fi
+								'''
+							}
+						}
+					}
+					stage('SRPM') {
+						steps {
+							script {
+								if (srpm_step != null) {
+									srpm_step()
+								} else {
+									dir('src') {
+										if (fileExists('setup.py')) {
+											sh '''
+												set -e
+												rm -rf build dist
+												if head -1 setup.py | grep -q python3 ; then
+													python=python3
+												else
+													python=python2
+												fi
+												$python setup.py sdist
+												$python setup.py bdist_rpm --spec-only
+												rpmbuild --define "_srcrpmdir ./" --define "_sourcedir dist/" -bs dist/*.spec
+												rm -rf build dist
+											'''
+										} else if (fileExists('pypipackage-to-srpm.yaml')) {
+											sh '''
+												y=pypipackage-to-srpm.yaml
+												url=$(shyaml get-value url < $y)
+												fn=$(basename "$url")
+												sha256sum=$(shyaml get-value sha256sum < $y)
+												mangle_name=
+												if [ "$(shyaml get-value mangle_name True < $y)" == "False" ] ; then
+													mangle_name=--no-mangle-name
+												fi
+												epoch=$(shyaml get-value epoch < $y || true)
+												if [ "$epoch" != "" ] ; then
+													epoch="--epoch=$epoch"
+												fi
+												python_versions=$(shyaml get-values python_versions < $y || true)
+												if [ "$python_versions" == "" ] ; then
+													python_versions="2 3"
+												fi
+												if [ "$python_versions" == "2 3" -o "$python_versions" == "3 2" ] ; then
+													if [ "$mangle_name" == "--no-mangle-name" ] ; then
+														>&2 echo error: cannot build for two Python versions without mangling the name of the package
+														exit 36
+													fi
+												fi
+												diffs=1
+												for f in *.diff ; do
+													test -f "$f" || diffs=0
+												done
+												wget --progress=dot:giga --timeout=15 -O "$fn" "$url"
+												actualsum=$(sha256sum "$fn" | cut -d ' ' -f 1)
+												if [ "$actualsum" != "$sha256sum" ] ; then
+													>&2 echo error: SHA256 sum "$actualsum" of file "$fn" does not match expected sum "$sha256sum"
+													exit 32
+												fi
+												for v in $python_versions ; do
+													if [ "$diffs" == "1" ] ; then
+														python"$v" `which pypipackage-to-srpm` --no-binary-rpms $epoch $mangle_name "$fn" *.diff
+													else
+														python"$v" `which pypipackage-to-srpm` --no-binary-rpms $epoch $mangle_name "$fn"
+													fi
+												done
+											'''
+										} else {
+											sh 'make srpm'
+										}
+									}
+								}
+							}
+						}
+					}
+					stage('RPMs') {
+						steps {
+							dir('out') {
+								sh 'rm -rf -- *'
+							}
+							script {
+								if (params.RELEASE != '') {
+									RELEASE = params.RELEASE
+								}
+								println "Building RPMs for Fedora releases ${RELEASE}"
+								parallel automockfedorarpms_all(RELEASE.split(' '))
+							}
+						}
+					}
+					stage('Stash') {
+						steps {
+							stash includes: 'out/*/*.rpm', name: 'out'
+						}
+					}
 				}
 			}
-			stage('RPMs') {
+			stage('Unstash') {
 				steps {
-					dir('out') {
-						sh 'rm -rf -- *'
+					dir("out") {
+						deleteDir()
 					}
-					script {
-						if (params.RELEASE != '') {
-							RELEASE = params.RELEASE
-						}
-						println "Building RPMs for Fedora releases ${RELEASE}"
-						parallel automockfedorarpms_all(RELEASE.split(' '))
-					}
+					unstash 'out'
 				}
 			}
 			stage('Publish') {
+				when {
+					expression {
+						return env.BRANCH_NAME == "master"
+					}
+				}
 				steps {
 					script {
-						if (env.BRANCH_NAME == "master") {
-							autouploadfedorarpms()
-						}
+						autouploadfedorarpms()
 					}
 				}
 			}
