6ba612ed2fab26353134f1b17c8559561978cef1
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2019-03-14 01:43:45 +0000

    setuptools

--- vars/genericFedoraRPMPipeline.groovy
+++ vars/genericFedoraRPMPipeline.groovy
@@ -166,7 +166,7 @@ def call(checkout_step = null, srpm_step = null) {
 					stage('Deps') {
 						steps {
 							script {
-								funcs.dnfInstall(['rpm-build', 'pypipackage-to-srpm', 'shyaml', 'python2-nose', 'python3-nose', 'python2', 'python3'])
+								funcs.dnfInstall(['rpm-build', 'pypipackage-to-srpm', 'shyaml', 'python2-nose', 'python3-nose', 'python2', 'python3', 'python2-setuptools', 'python3-setuptools'])
 							}
 						}
 					}
