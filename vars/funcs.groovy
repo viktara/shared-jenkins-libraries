a5f900d2f6b9c3c8b9f8b802cbba36fd27d86f49
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2019-06-11 16:32:46 +0000

    userContent drivers.

--- vars/funcs.groovy
+++ vars/funcs.groovy
@@ -101,24 +101,24 @@ def aptEnableSrc() {
 }
 
 def announceBeginning() {
-    sh """
+    sh '''
        test -x /usr/local/bin/announce-build-result && f=/usr/local/bin/announce-build-result || test -f /var/lib/jenkins/userContent/announce-build-result && f=/var/lib/jenkins/userContent/announce-build-result || exit 0
        $f has begun
-       """
+       '''
 }
 
 def announceEnd(status) {
-    sh """
+    sh '''
        test -x /usr/local/bin/announce-build-result && f=/usr/local/bin/announce-build-result || test -f /var/lib/jenkins/userContent/announce-build-result && f=/var/lib/jenkins/userContent/announce-build-result || exit 0
        $f finished with status ${status}
-       """
+       '''
 }
 
 def uploadDeliverables(spec) {
-    sh """
+    sh '''
        test -x /usr/local/bin/announce-build-result && f=/usr/local/bin/announce-build-result || test -f /var/lib/jenkins/userContent/announce-build-result && f=/var/lib/jenkins/userContent/announce-build-result || exit 0
        $f ${spec}
-       """
+       '''
 }
 
 def describeCause(currentBuild) {
