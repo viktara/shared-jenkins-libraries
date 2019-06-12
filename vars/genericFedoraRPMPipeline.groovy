5785dea2ca0604c695feb06c17712ee1af276eb4
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2019-06-12 09:15:17 +0000

    Distinguish between archive and publish.

--- vars/genericFedoraRPMPipeline.groovy
+++ vars/genericFedoraRPMPipeline.groovy
@@ -325,6 +325,15 @@ def call(checkout_step = null, srpm_step = null, srpm_deps = null) {
 					unstash 'out'
 				}
 			}
+			stage('Archive') {
+				steps {
+					script {
+						def outputs = autolistrpms().collect{ funcs.wrapLi(funcs.escapeXml(it)) }.join("\n")
+						currentBuild.description = "<p>Outputs:</p>" + funcs.wrapUl(outputs)
+						archiveArtifacts artifacts: 'out/*/*.rpm', fingerprint: true
+					}
+				}
+			}
 			stage('Publish') {
 				when {
 					expression {
@@ -333,9 +342,6 @@ def call(checkout_step = null, srpm_step = null, srpm_deps = null) {
 				}
 				steps {
 					script {
-						def outputs = autolistrpms().collect{ funcs.wrapLi(funcs.escapeXml(it)) }.join("\n")
-						currentBuild.description = "<p>Outputs:</p>" + funcs.wrapUl(outputs)
-						archiveArtifacts artifacts: 'out/*/*.rpm', fingerprint: true
 						autouploadfedorarpms()
 					}
 				}
