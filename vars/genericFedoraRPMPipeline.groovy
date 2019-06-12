786ed0199d3a1a22f0531cb0d6def77aa8a61729
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2019-06-12 00:59:32 +0000

    Refactor.

--- vars/genericFedoraRPMPipeline.groovy
+++ vars/genericFedoraRPMPipeline.groovy
@@ -282,38 +282,7 @@ def call(checkout_step = null, srpm_step = null, srpm_deps = null) {
 										} else if (fileExists('pypipackage-to-srpm.yaml')) {
 											script {
 												def basename = funcs.downloadPypiPackageToSrpmSource()
-												sh """
-												y=pypipackage-to-srpm.yaml
-												mangle_name=
-												if [ "\$(shyaml get-value mangle_name True < \$y)" == "False" ] ; then
-													mangle_name=--no-mangle-name
-												fi
-												epoch=\$(shyaml get-value epoch '' < \$y || true)
-												if [ "\$epoch" != "" ] ; then
-													epoch="--epoch=\$epoch"
-												fi
-												python_versions=\$(shyaml get-values python_versions < \$y || true)
-												if [ "\$python_versions" == "" ] ; then
-													python_versions="2 3"
-												fi
-												if [ "\$python_versions" == "2 3" -o "\$python_versions" == "3 2" ] ; then
-													if [ "\$mangle_name" == "--no-mangle-name" ] ; then
-														>&2 echo error: cannot build for two Python versions without mangling the name of the package
-														exit 36
-													fi
-												fi
-												diffs=1
-												for f in *.diff ; do
-													test -f "\$f" || diffs=0
-												done
-												for v in \$python_versions ; do
-													if [ "\$diffs" == "1" ] ; then
-														python"\$v" `which pypipackage-to-srpm` --no-binary-rpms \$epoch \$mangle_name "${basename}" *.diff
-													else
-														python"\$v" `which pypipackage-to-srpm` --no-binary-rpms \$epoch \$mangle_name "${basename}"
-													fi
-												done
-												"""
+												funcs.buildDownloadedPypiPackage(basename)
 											}
 										} else {
 											sh 'make srpm'
